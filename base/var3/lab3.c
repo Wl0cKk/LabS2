2485ed8c47e6fb50a2fd98aa915ba4aabc17e953
Wl0cKk <kandraciuk404@gmail.com>
2024-04-13 18:58:18 +0300

    lab3 chapter 2 task1 var3

--- base/var3/lab3.c
+++ base/var3/lab3.c
@@ -1,17 +1,28 @@
 #include "stdio.h"
 
 int recursion(int m, int n) {
-    
+    if (n == 0) {
+        return m;
+    } else {
+        return recursion(n, m % n);
+    }
+    return m;
 }
 
 int iteration(int m, int n) {
-    
+    int r;
+    while (n != 0) {
+        r = n;
+        n = m % n;
+        m = r;
+    }
+    return m;
 }
 
 int main() {
-    int m = 60;
-    int n = 20;
-    printf("Recursive function result: %d\n", recursion(arr, 0, l-1, s));
-    printf("Iterative function result: %d\n", iteration(arr, l, s));
+    int m = 660;
+    int n = 10;
+    printf("Recursive function result: %d\n", recursion(m, n));
+    printf("Iterative function result: %d\n", iteration(m, n));
     return 0;
 }
