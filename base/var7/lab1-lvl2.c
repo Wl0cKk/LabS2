a756139f9d5fc1e387fb5637c3cf2c2e002c6975
Wl0cKk <kandraciuk404@gmail.com>
2024-05-28 11:28:43 +0300

    fix lab 1 var 7 & 8

--- base/var7/lab1-lvl2.c
+++ base/var7/lab1-lvl2.c
@@ -1,22 +1,17 @@
-#include "stdio.h"
-#include "string.h"
+#include <stdio.h>
+#include <string.h>
+
+const char *str = "eogng -4343.534 -76,54.429wrguih5632dff787w,,,,,,.545345.3536.33323wegw45y.35+3434f2wef.45-fef43";
+const int fraction = 2;
 
 int main() {
-    char str[] = "eogng -7654,456787,,,,,,.545345.3536.3wegw45y.35+3434f2wef.45-fef43";
-    int precision = 2;
-
-    char cleanedStr[strlen(str)];
-    int j = 0;
-
-    for (int i = 0; str[i]; i++) {
-        if ((str[i] >= '0' && str[i] <= '9') || str[i] == '.' || str[i] == '+' || str[i] == '-') {
-            cleanedStr[j++] = str[i];
+    int len = strlen(str);
+    for (int i = 0; i < len; ++i) {
+        double d;
+        if (sscanf(&str[i], "%lf", &d) > 0) {
+            printf("%.*lf", fraction, d);
+            break;
         }
     }
-    cleanedStr[j] = '\0';
-    double d;
-    if (sscanf(cleanedStr, "%lf", &d) > 0) {
-        printf("%.*lf", precision, d);
-    } 
     return 0;
 }
--- base/var8/lab1-lvl2.c
+++ base/var8/lab1-lvl2.c
@@ -1,21 +1,27 @@
-#include "stdio.h"
-#include "string.h"
+#include <stdio.h>
+#include <string.h>
 
-int main() {
-    char str[] = "eogng -7654,456787,,,,,,.545345.3536.3wegw45y.35+3434f2wef.45-fef43";
-
-    char cleanedStr[strlen(str)];
-    int j = 0;
-
-    for (int i = 0; str[i]; i++) {
-        if ((str[i] >= '0' && str[i] <= '9') || str[i] == '.' || str[i] == '+' || str[i] == '-') {
-            cleanedStr[j++] = str[i];
+const char *str = "eogng-4343,534-76,54.429wrguih5632dff787w,,,,,,.545345.3536.33323wegw45y.35+3434f2wef.45-fef43";
+int main()
+{
+    int len = strlen(str);
+    for (int i = 0; i < len; ++i) {
+        double d;
+        if (sscanf(&str[i], "%lf", &d) > 0) {
+            int j = i + 1;
+            int pc = 0;
+            while (j < len && ((str[j] >= '0' && str[j] <= '9') || str[j] == '.' || str[j] == ',')) {
+                if (str[j] == '.' || str[j] == ',') ++pc;
+                if (pc > 1) break;
+                ++j;
+            }
+            int substr_len = j - i;
+            char buf[substr_len + 1];
+            strncpy(buf, &str[i], substr_len);
+            buf[substr_len] = 0;
+            printf("%s\n", buf);
+            break;
         }
     }
-    cleanedStr[j] = '\0';
-    double d;
-    if (sscanf(cleanedStr, "%lf", &d) > 0) {
-        printf("%lf", d);
-    } 
     return 0;
 }
