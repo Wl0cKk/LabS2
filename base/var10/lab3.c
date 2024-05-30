2e337a29587c1ed865d4ea02275b817dc2fbcc8c
Wl0cKk <kandraciuk404@gmail.com>
2024-05-31 01:05:19 +0300

    lab3 var 10

--- base/var10/lab3.c
+++ base/var10/lab3.c
@@ -1,25 +1,72 @@
-#include "stdio.h"
-#include "math.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
-double recursive(int n, double res) {
-    if (n == 0) {
-        return res;
-    } else {
-        return recursive(n - 1, sqrt(n + res));
+int recursive(int *arr, int start, int end) {
+    if (start == end) {
+        return arr[end];
     }
+
+    int mid = (start + end) / 2;
+
+    int max1 = recursive(arr, start, mid);
+    int max2 = recursive(arr, mid+1, end);
+
+    return (max1 > max2) ? max1 : max2;
 }
 
-double iterative(int n) {
-    double res = 0.0;
-    for (int i = n; i > 0; --i) {
-        res = sqrt(i + res);
+int iterative(int *arr, int n) {
+    int start = 0;
+    int end = n - 1;
+
+    while (start < end) {
+        int mid = start + (end - start) / 2;
+        int max1 = arr[start];
+        for (int i = start + 1; i <= mid; i++) {
+            if (arr[i] > max1) {
+                max1 = arr[i];
+            }
+        }
+        int max2 = arr[mid + 1];
+        for (int i = mid + 2; i <= end; i++) {
+            if (arr[i] > max2) {
+                max2 = arr[i];
+            }
+        }
+        if (max1 > max2) {
+            end = mid;
+        } else {
+            start = mid + 1;
+        }
     }
-    return res;
+
+    return arr[end];
+}
+
+int* generateRandomArray(int n) {
+    srand(time(NULL));
+    int* arr = (int*)malloc(sizeof(int) * n);
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 201 - 100;
+    }
+
+    return arr;
 }
 
 int main() {
-    int n = 110;
-    printf("Recursive n = %d: %.52lf\n", n, recursive(n, 0.0));
-    printf("Iterative n = %d: %.52lf\n", n, iterative(n));
+    int n = 20;
+    int *arr = generateRandomArray(n);
+    
+    printf("\nArray:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    puts("");
+    printf("\nRecursive n = %d: MAX: %d\n", n, recursive(arr, 0, n - 1));
+    printf("\nIterative n = %d: MAX: %d\n\n", n, iterative(arr, n));
+    
+    free(arr);
     return 0;
 }
