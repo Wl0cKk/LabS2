fb02bb920c3b31b02e710931ba16c37f8c44720f
Wl0cKk <kandraciuk404@gmail.com>
2024-06-05 02:26:36 +0300

    var 6 lab 4

--- base/var6/lab4.c
+++ base/var6/lab4.c
@@ -1,40 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <memory.h>
+#include <stdbool.h>
 
 typedef struct {
-    int data[100];
+    int data[10];
     int top;
-} UintStack;
+} IntStack;
 
-void init_stack(UintStack* stack) {
-    memset(stack->data, 0, sizeof(stack->data));
+
+bool stack_push(IntStack* stack, int v);
+
+void init_stack(IntStack* stack) {
+    //memset(stack->data, 0, sizeof(stack->data));
     stack->top = 0;
+    for (int i = 0; i < 10; ++i) {
+        stack_push(stack, rand() % 21 - 10);
+    }
 }
-
-int stack_push(UintStack* stack, int v) {
-    if (stack->top >= 100) {
-        return 0;
+bool stack_push(IntStack* stack, int v) {
+    if (stack->top >= 10) {
+        return false;
     }
     stack->data[stack->top++] = v;
-    return 1;
+    return true;
 }
-
-int stack_pop(UintStack* stack) {
+int stack_pop(IntStack* stack) {
     if (stack->top <= 0) {
         return -1;
     }
     return stack->data[--stack->top];
 }
 
-void print_stack(UintStack* stack) {
+void sort_stack(IntStack* stack) {
+    if (stack->top <= 0) {
+        return;
+    }
+    for (int i = 0; i < stack->top - 1; ++i) {
+        for (int j = i + 1; j < stack->top; ++j) {
+            if (stack->data[i] > stack->data[j]) {
+                int temp = stack->data[i];
+                stack->data[i] = stack->data[j];
+                stack->data[j] = temp;
+            }
+        }
+
+    }
+}
+
+void print_stack(IntStack* stack) {
     for (int i = 0; i < stack->top; ++i) {
         printf("%d ", stack->data[i]);
     }
     printf("\n");
 }
-
-void task_6(UintStack* stack) {
+void task_6(IntStack* stack) {
     int minIndex = 0;
     int maxIndex = 0;
     for (int i = 0; i < stack->top; ++i) {
@@ -49,17 +69,16 @@ void task_6(UintStack* stack) {
     stack->data[minIndex] = stack->data[maxIndex];
     stack->data[maxIndex] = temp;
 }
-
 int main() {
-    UintStack s;
+    IntStack s;
     init_stack(&s);
-    stack_push(&s, 42);
-    stack_push(&s, 100);
-    stack_push(&s, 5);
-    stack_push(&s, 7);
     print_stack(&s);
+
+    sort_stack(&s);
+    print_stack(&s);
+
     task_6(&s);
     print_stack(&s);
+
     return 0;
 }
-
